b09b995e2a94a987c550b60998e2eac7b9ba2564
inkarri <jhanpieremontes@gmail.com>
2023-05-31 19:15:00 -0500

    Se agregaron mejoras respecto a la estructura en HTML, estilo en CSS y logica en JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,52 +1,49 @@
-function encryptText() {
-    // Obtener el texto de entrada
-    var input = document.getElementById('inputText').value;
+const textArea = document.querySelector(".text-area")
+const message = document.querySelector(".message")
+const copyText = document.querySelector(".copy-text")
+const button = document.querySelector(".btn-encrypt")
+const listVowels = ["a","e","i","o","u"]
+const listEncryptionKeys = ["ai", "enter", "imes", "ober", "ufat"]
 
-    // Reemplazar las vocales por 'mimi'
-    var encryptedText = input
+function decitionCrypt() {
+    if (button.classList.contains("reverse")) {
+        decryptText()
+    }
+    else {
+        encryptText()
+    }
+}
+
+function encryptText() {
+    message.textContent = textArea.value
         .replace(/e/gi, 'enter')
         .replace(/i/gi, 'imes')
         .replace(/a/gi, 'ai')
         .replace(/o/gi, 'ober')
         .replace(/u/gi, 'ufat');
-
-    // Mostrar el texto encriptado en el div de salida
-    var outputDiv = document.getElementById('outputText');
-    outputDiv.textContent = encryptedText;
-
-    // Habilitar el botón de copiar
-    var copyButton = document.getElementById('copyButton');
 }
 
-function copyText() {
-    // Obtener el texto encriptado
-    var encryptedText = document.getElementById('outputText').textContent;
 
-    // Crear un elemento de textarea temporal
-    var tempTextarea = document.createElement('textarea');
-    tempTextarea.value = encryptedText;
+function toggleHeaderEncryption() {
+    let headerEncrypt = document.querySelector(".header-container")
 
-    // Agregar el elemento de textarea temporal al documento
-    document.body.appendChild(tempTextarea);
+    headerEncrypt.classList.toggle("reverse")
+    button.classList.toggle("reverse");
 
-    // Seleccionar el texto del textarea
-    tempTextarea.select();
-    tempTextarea.setSelectionRange(0, 99999); // Para dispositivos móviles
-
-    // Copiar el texto al portapapeles
-    document.execCommand('copy');
-
-    // Eliminar el elemento de textarea temporal
-    document.body.removeChild(tempTextarea);
-
-    // Cambiar el estado del botón de copiar
-    var copyButton = document.getElementById('copyButton');
-    copyButton.innerHTML = '<img src="./assets/copy.svg" alt="copiar">';
-    copyButton.disabled = true;
-
-    // Restaurar el estado del botón después de 2 segundos
-    setTimeout(function() {
-        copyButton.innerHTML = '<img src="./assets/copy.svg" alt="copiar">';
-        copyButton.disabled = false;
-    }, 2000);
+    if (button.classList.contains("reverse")) {
+        button.innerHTML = '<img src="../assets/unlock.svg" alt=""><p>Desencriptar</p>';
+    } else {
+        button.innerHTML = '<img src="../assets/lock.svg" alt=""><p>Encriptar</p>';
+    }
 }
+
+function decryptText() {
+    message.textContent = textArea.value
+        .replace(/enter/gi, 'e')
+        .replace(/imes/gi, 'i')
+        .replace(/ai/gi, 'a')
+        .replace(/ober/gi, 'o')
+        .replace(/ufat/gi, 'u');
+}
+
+
